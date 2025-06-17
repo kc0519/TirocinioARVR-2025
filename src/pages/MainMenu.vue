46ea5d647210c8b823dab9266d84e7a5a2a6baae
kc0519 <chrikina05@gmail.com>
2025-06-17 16:34:05 +0200

    bo21

--- src/pages/MainMenu.vue
+++ src/pages/MainMenu.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
     import GameButtons from "@/components/ui/HUD/GameButtons.vue";
+    import GameLobby from "@/components/ui/gameLobby.vue";
 </script>
 
 <template>
-    <div>
-        <GameButtons class="start-game-button button">
-            Nuova Partita
-        </GameButtons>
-        <GameButtons class="start-tutorial-button button">
-            Tutorial
-        </GameButtons>
+    <div class="main-menu">
+        <div class="buttons-container">
+            <GameButtons class="start-game-button button">
+                Nuova Partita
+            </GameButtons>
+            <GameButtons class="start-tutorial-button button">
+                Tutorial
+            </GameButtons>
+        </div>
+        <GameLobby />
     </div>
 </template>
 
 <style lang="scss" scoped>
+    .buttons-container {
+        width: 100%;
+    }
+    .main-menu {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        background-image: url('../../public/img/MainMenuBackground.png');
+        background-size: cover;
+    }
+
     .button{
         position: absolute;
         width: 20%;
